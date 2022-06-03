0946a14c87976cb239a5cbcac729518b957d1483
Tatyana Izmailova <rutaizm@yandex.ru>
2022-06-03 16:31:30 +0300

    deleted marking

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -13,35 +13,6 @@ function PopupWithForm({isOpen, name, title, children, onClose, buttonTitle}) {
                 </form>
             </div>
         </div>
-        {/* <div className={`pop-up popup-photo popup_type_${name}`}>
-            <div className="pop-up__container">
-                <form className="edit-form edit-form_type_photo" name={name} noValidate>
-                    <button className="pop-up__close popup-photo__close-button" type="button"></button>
-                    <h2 className="edit-form__title">{title}</h2>
-                    {children}
-                    <button className="pop-up__save popup-photo__save-button" type="submit">Создать</button>
-                </form>
-            </div>
-        </div>
-        <div className={`pop-up confirmation-popup popup_type_${name}`}>
-            <div className= "pop-up__container confirmation-popup__container">
-                <form className="edit-form" name={name} noValidate>
-                    <button className="pop-up__close" type="button"></button>
-                    <h2 className="edit-form__title">Вы уверены?</h2>
-                    <button className="pop-up__save" type="submit">Да</button>
-                </form>    
-            </div>
-        </div>
-        <div className={`pop-up new-avatar-popup popup_type_${name}`}>
-            <div className="pop-up__container">
-                <form className="edit-form edit-form_new-avatar" name={name} noValidate>
-                    <button className="pop-up__close" type="button"></button>
-                    <h2 className="edit-form__title">{title}</h2>
-                    {children}
-                    <button className="pop-up__save" type="submit">Сохранить</button>
-                </form>
-            </div>
-        </div> */}
     </>  
     )
 }
