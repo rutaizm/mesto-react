9720194720f9c13ae0d1b6bf050b2e0176a50266
Tatyana Izmailova <rutaizm@yandex.ru>
2022-06-17 14:30:20 +0300

    added context in card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,16 @@
 import React from "react";
+import {CurrentUserContext} from '../contexts/CurrentUserContext'
  
 function Card({card, onCardClick}) {
+
+    const currentUser = React.useContext(CurrentUserContext);
+
+    const isOwn = card.owner._id === currentUser._id;
+    const isLiked = card.likes.some(i => i._id === currentUser._id);
+
+    const cardDeleteButtonClassName = (`element__delete-button ${isOwn ? 'element__delete-button_visible' : 'element__delete-button_hidden'}`); 
+    const cardLikeButtonClassName = isLiked ? "element__like-button_active" : "element__like-button";
+
 
     function handleClick() {
         onCardClick(card);
@@ -13,7 +23,7 @@ function Card({card, onCardClick}) {
          <div className="element__item">
              <p className="element__title">{card.name}</p>
                  <div className="element__like-container">
-                     <button type="button" className="element__like-button"></button>
+                     <button type="button" className={cardLikeButtonClassName}></button>
                      <span className="element__like-counter">{card.likes.length}</span>
                  </div>    
          </div>
